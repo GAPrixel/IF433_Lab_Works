80573873d5828c8121f3ded30942886e91f46e02
GAPrixel <athaqighazy@gmail.com>
2026-02-09 14:41:11 +0700

    week01: refactor to use val and string templates

--- src/OOP_82331_GhazyAthaqiSupriadi/week1/GradingSystem.kt
+++ src/OOP_82331_GhazyAthaqiSupriadi/week1/GradingSystem.kt
@@ -1,4 +1,10 @@
 package OOP_82331_GhazyAthaqiSupriadi.week1
 
-class GradingSystem {
+fun main() {
+    // REFACTOR: Gunakan val dan hapus tipe data eksplisit
+    val name = "John Thor"
+    val score = 80
+
+    // REFACTOR: String Template ($name)
+    println("Nama: $name, Nilai: $score")
 }
