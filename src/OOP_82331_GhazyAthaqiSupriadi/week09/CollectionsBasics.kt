42048455e4006edd86b750107875420fc9f86485
GAPrixel <athaqighazy@gmail.com>
2026-04-24 10:25:28 +0700

    week09: implement set to discard duplicate elements

--- src/OOP_82331_GhazyAthaqiSupriadi/week09/CollectionsBasics.kt
+++ src/OOP_82331_GhazyAthaqiSupriadi/week09/CollectionsBasics.kt
@@ -1,4 +1,14 @@
 package OOP_82331_GhazyAthaqiSupriadi.week09
 
-class CollectionsBasics {
+
+fun main() {
+    // LANGKAH 2: Set (Keunikan Data)
+    println("\n=== TEST SET ===")
+    val uniqueNumbers = setOf(1, 2, 2, 3, 3, 4)
+    println("Unique Numbers (Set): $uniqueNumbers") // Duplikat hilang
+
+    val activeUsers = mutableSetOf("UserA", "UserB")
+    activeUsers.add("UserC")
+    activeUsers.add("UserA") // Diabaikan karena sudah ada
+    println("Active Users: $activeUsers")
 }
