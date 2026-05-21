57c29457daea2d5e5d5f894a78b73dbd6f81caa4
GAPrixel <athaqighazy@gmail.com>
2026-05-21 08:30:38 +0700

    week12: implement onSuccess and onFailure chaining

--- src/OOP_82331_GhazyAthaqiSupriadi/week12/RunCatchingExample.kt
+++ src/OOP_82331_GhazyAthaqiSupriadi/week12/RunCatchingExample.kt
@@ -5,12 +5,23 @@ fun main() {
     val result: Result<Int> = runCatching {
         "42X".toInt()
     }
-}
-
-// Pattern: getOrElse
-val safeValue = result.getOrElse { -1 }
-println("Safe Value (getOrElse): $safeValue")
+    // Pattern: getOrElse
+    val safeValue = result.getOrElse { -1 }
+    println("Safe Value (getOrElse): $safeValue")
 
 // Pattern: recover (bisa mengubah tipe kegagalan jadi sukses)
-val recovered = result.recover { 0 }.getOrNull()
-println("Recovered Value: $recovered")
+    val recovered = result.recover { 0 }.getOrNull()
+    println("Recovered Value: $recovered")
+
+    runCatching {
+        "100".toInt()
+    }.onSuccess { v ->
+        println("Berhasil dikonversi: $v")
+    }.onFailure { e ->
+        println("Gagal konversi: ${e.message}")
+    }
+}
+
+
+
+
