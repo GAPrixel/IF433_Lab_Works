363544cd05ae571d3ff8813f03cff59b9233bcce
GAPrixel <athaqighazy@gmail.com>
2026-05-21 09:00:06 +0700

    week12: (task) setup SmartFeeder main function

--- src/OOP_82331_GhazyAthaqiSupriadi/week12/FeederExceptions.kt
+++ src/OOP_82331_GhazyAthaqiSupriadi/week12/FeederExceptions.kt
@@ -7,26 +7,3 @@ class FoodEmptyException(requested: Int, available: Int) :
 
 class DispenserJamException :
     FeederException("Wadah dispenser tersangkut/macet!")
-
-fun dispenseKibble(
-    requestedGram: Int,
-    availableGram: Int,
-    isJammed: Boolean
-): Int {
-
-    require(requestedGram > 0) {
-        "Porsi kibble harus lebih dari 0 gr"
-    }
-
-    if (isJammed) {
-        throw DispenserJamException()
-    }
-
-    if (requestedGram > availableGram) {
-        throw FoodEmptyException(requestedGram, availableGram)
-    }
-
-    println("Kibble berhasil dikeluarkan!")
-
-    return availableGram - requestedGram
-}
